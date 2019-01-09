75036f2e8f75f475d7fc6aeb9d119f3b3cf90736
Nic Ferrier <nic@ferrier.me.uk>
2019-01-09 20:34:31 +0000

    display the issue time in the list

--- www/script.js
+++ www/script.js
@@ -1,3 +1,41 @@
+function mkel(name, parent) {
+    if (parent !== undefined) {
+        return parent.appendChild(document.createElement(name));
+    }
+    else {
+        return document.createElement(name);
+    }
+}
+
+function narrowingDate(time) {
+    const getMonthString = function (time) {
+        console.log("getMonth");
+        const years = time.getFullYear();
+        const months = "" + time.getMonth() + 1;
+        return `${years}-${months.padStart(2, "0")}`;
+    };
+    const getDayString = function (time) {
+        console.log("getDay");
+        const months = "" + time.getMonth() + 1;
+        const days = "" + time.getDate();
+        return `${months.padStart(2, "0")}-${days.padStart(2, "0")}`;
+    };
+    const now = new Date();
+    const hours = "" + time.getHours();
+    const minutes = "" + time.getMinutes();
+    const timeString = `${hours.padStart(2, "0")}:${minutes.padStart(2, "0")}`;
+
+    const epoch = time.valueOf();
+    const oneDayMs = (1000 * 60 * 60 * 24);
+    const oneYearMs = (1000 * 60 * 60 * 24 * 365);
+    const timeMs = now.valueOf();
+    const longString = (epoch < timeMs - oneYearMs)
+          ? getMonthString(time)
+          : (epoch < timeMs - oneDayMs)
+          ? getDayString(time)
+          : timeString;
+    return longString;
+}
 
 window.addEventListener("load", async loadEvt => {
     console.log(`
@@ -30,23 +68,38 @@ window.addEventListener("load", async loadEvt => {
     const baseUrl = document.location.href;
     const response = await fetch(baseUrl + "/top");
     const jsonData = await response.json();
-    const data = JSON.parse(jsonData);
+    const data = jsonData; // JSON.parse(jsonData);
     console.log(data);
     const issueElements = data.map(issue => {
-        const {id,d,data: {summary, description, editor}} = issue;
-        const issueElement = document.createElement("table");
-        const tr = issueElement.appendChild(document.createElement("tr"));
-        const tdSummary = tr.appendChild(document.createElement("td"));
+        const {id,d,last_update,data: {summary, description, editor}} = issue;
+        const tr = mkel("tr");
+        const tdDate = mkel("td", tr);
+        tdDate.classList.add("date");
+        const date = narrowingDate(new Date(last_update));
+        tdDate.textContent = date;
+        const tdSummary = mkel("td", tr);
         tdSummary.classList.add("summary");
         tdSummary.textContent = summary;
-        const tdDescription = tr.appendChild(document.createElement("td"));
+        const tdDescription = mkel("td", tr);
         tdDescription.classList.add("description");
         tdDescription.textContent = description;
-        return issueElement;
+        return tr;
     });
 
     const top = document.querySelector(".top-issues");
+    const table = mkel("table", top);
+    const head = mkel("thead", table);
+    const date = mkel("th", head);
+    date.classList.add("date");
+    date.textContent = "updated";
+    const summary = mkel("th", head);
+    summary.classList.add("summary");
+    summary.textContent = "summary";
+    const description = mkel("th", head);
+    description.classList.add("description");
+    description.textContent = "description";
+
     issueElements.forEach(issue => {
-        top.appendChild(issue);
+        table.appendChild(issue);
     });
 });
