2ffb9f1669318a0b192ebb52aa5d328e6e1397bf
Nic Ferrier <nic@ferrier.me.uk>
2019-02-28 09:36:27 +0000

    add owner display

--- www/script.js
+++ www/script.js
@@ -26,7 +26,20 @@ window.addEventListener("load", async loadEvt => {
 |___| |_______||_______||_______||_______||_______|
 `);
     console.log("an issue tracking system without any bells and no whistles at all.");
-    const user = "nicferrier";
+
+    // Now get some data
+    const baseUrl = document.location.href;
+
+    // First the context
+    const ctxResponse = await fetch(baseUrl + "/context");
+    const {authData:{email}} = await ctxResponse.json();
+    const userButton = document.querySelector("button#user");
+    userButton.textContent = email;
+    userButton.addEventListener("click", clickEvt => {
+        alert("nothing happens when you click this right now!");
+    });
+    const user = email;
+    
     document.querySelector("form").addEventListener("submit", async submitEvt => {
         submitEvt.preventDefault();
         submitEvt.stopPropagation();
@@ -43,17 +56,6 @@ window.addEventListener("load", async loadEvt => {
         return false;
     });
 
-    // Now get some data
-    const baseUrl = document.location.href;
-
-    // First the context
-    const ctxResponse = await fetch(baseUrl + "/context");
-    const {authData:{email}} = await ctxResponse.json();
-    const userButton = document.querySelector("button#user");
-    userButton.textContent = email;
-    userButton.addEventListener("click", clickEvt => {
-        alert("nothing happens when you click this right now!");
-    });
 
     // Now the top issues
     const response = await fetch(baseUrl + "/top");
@@ -63,38 +65,57 @@ window.addEventListener("load", async loadEvt => {
 
     const issueElements = data.map(issue => {
         const {id, d, last_update, state, data: {
-            issueid, summary, description, editor
+            issueid, summary, description, editor:owner
         }} = issue;
+
         const tr = mkel("tr");
         tr.id = issueid;
+
         const tdDate = mkel("td", tr);
         tdDate.classList.add("date");
+
         const date = narrowingDate(new Date(last_update));
         tdDate.textContent = date;
+
+        const tdOwner = mkel("td", tr);
+        tdOwner.classList.add("owner");
+        tdOwner.textContent = owner;
+
         const tdState = mkel("td", tr);
         tdState.classList.add("state");
         tdState.textContent = state;
+
         const tdSummary = mkel("td", tr);
         tdSummary.classList.add("summary");
         tdSummary.textContent = summary;
+
         const tdDescription = mkel("td", tr);
         tdDescription.classList.add("description");
         tdDescription.textContent = description;
+
         return tr;
     });
 
     const top = document.querySelector(".top-issues");
     const table = mkel("table", top);
     const head = mkel("thead", table);
+
     const date = mkel("th", head);
     date.classList.add("date");
     date.textContent = "updated";
+
+    const owner = mkel("th", head);
+    owner.classList.add("owner");
+    owner.textContent = "owner";
+
     const state = mkel("th", head);
     state.classList.add("state");
     state.textContent = "state";
+
     const summary = mkel("th", head);
     summary.classList.add("summary");
     summary.textContent = "summary";
+
     const description = mkel("th", head);
     description.classList.add("description");
     description.textContent = "description";
